0c916fd46fd80050eab55b55aaf6dafd756d71a0
HarshadMahajan <50816401+HarshadMahajan@users.noreply.github.com>
2020-11-24 17:38:44 -0500

    Add files via upload

---
+++ Matrix/matrixDiagonal.py
@@ -0,0 +1,55 @@
+ROW = 5
+COL = 4
+  
+# Main function that prints given 
+# matrix in diagonal order 
+def rightDiagonalOrder(matrix) : 
+      
+    # There will be ROW+COL-1 lines in the output 
+    for line in range(1, (ROW + COL)) : 
+        # Get column index of the first element 
+        # in this line of output. The index is 0 
+        # for first ROW lines and line - ROW for 
+        # remaining lines  
+        start_col = max(0, line - ROW) 
+  
+        # Get count of elements in this line. 
+        # The count of elements is equal to 
+        # minimum of line number, COL-start_col and ROW  
+        count = min(line, (COL - start_col), ROW) 
+  
+        # Print elements of this line  
+        for j in range(0, count) : 
+            print(matrix[min(ROW, line) - j - 1] 
+                        [start_col + j], end = "\t") 
+  
+        print() 
+      
+def leftDiagonalOrder(matrix):
+   for slice in range(2 * ROW - 1, 0, -1):
+       z = 0 if slice < ROW else slice - ROW +1
+       for j in range(slice-z,z,-1):
+           print(matrix[j][slice-j], end = "\t")
+       print()
+       
+# Utility function to print a matrix 
+def printMatrix(matrix) : 
+    for i in range(0, ROW) : 
+        for j in range(0, COL) : 
+            print(matrix[i][j], end = "\t") 
+              
+        print() 
+          
+  
+# Driver COde 
+M = [ [1, 2, 3, 4], 
+    [5, 6, 7, 8], 
+    [9, 10, 11, 12], 
+    [13, 14, 15, 16], 
+    [17, 18, 19, 20] ] 
+print("Given matrix is ") 
+printMatrix(M) 
+  
+print ("\nDiagonal printing of matrix is ") 
+rightDiagonalOrder(M)
+leftDiagonalOrder(M) 
